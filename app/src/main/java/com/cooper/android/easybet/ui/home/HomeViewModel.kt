6d197b590ef3a2bf746bc52548470cbe7dec7805
csmith845 <89327794+csmith845@users.noreply.github.com>
2022-03-26 20:09:56 -0500

    Revert "Revert "Cooper""

--- app/src/main/java/com/cooper/android/easybet/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/cooper/android/easybet/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is where active bets will be"
+        value = "Bet"
     }
     val text: LiveData<String> = _text
 }
