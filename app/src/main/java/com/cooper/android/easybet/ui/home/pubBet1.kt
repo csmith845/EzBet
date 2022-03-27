cdc55426a5937e89de7de2225b0db5a2db83e68b
easybet <cs7910@live.missouristate.edu>
2022-03-26 20:34:19 -0500

    semi working

--- app/src/main/java/com/cooper/android/easybet/ui/home/pubBet1.kt
+++ app/src/main/java/com/cooper/android/easybet/ui/home/pubBet1.kt
@@ -1,12 +1,22 @@
 package com.cooper.android.easybet.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.cooper.android.easybet.Bet
 import com.cooper.android.easybet.R
+import com.google.android.material.internal.ContextUtils.getActivity
 
 class pubBet1 : AppCompatActivity() {
+    private lateinit var newView: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pub_bet1)
+        newView = findViewById(R.id.Button_pub1)
+        newView.setOnClickListener{
+            val intent = Intent(this, Bet::class.java)
+            startActivity(intent)
+        }
     }
 }
