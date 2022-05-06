91b452c6aa12096da8ce26e315d67f1234f5477c
ZachReynolds2000 <zcr0907@live.missouristate.edu>
2022-05-05 23:00:51 -0500

    FINALLY WORKING

--- app/src/main/java/com/cooper/android/easybet/MainActivity.kt
+++ app/src/main/java/com/cooper/android/easybet/MainActivity.kt
@@ -72,6 +72,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WalletRepository.getBal()
 
         auth = FirebaseAuth.getInstance()
         if(!checkUser()){
