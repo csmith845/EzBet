39df1081125b2821b92474a41c9e9995f1f4b667
ZachReynolds2000 <zcr0907@live.missouristate.edu>
2022-05-09 18:08:43 -0500

    ic updates on menu

--- app/src/main/java/com/cooper/android/easybet/Bet.kt
+++ app/src/main/java/com/cooper/android/easybet/Bet.kt
@@ -2,20 +2,10 @@ package com.cooper.android.easybet
 
 import java.util.*
 
-data class Bets(private var id:UUID = UUID.randomUUID(), private var title:String ="", private var pot:Int = 0, private var friend1:String="", private var friend2 :String = "") {
-    fun getTitle(): String {
-        return title
-    }
+data class Bets(var id:UUID = UUID.randomUUID(),  var title:String ="",  var pot:Int = 0,  var friend1:String="",  var friend2 :String = "") {
 
-    fun getPot(): Int {
-        return pot
-    }
-    fun getFriend1():String{
-        return friend1
-    }
-    fun getFriend2():String{
-        return friend2
-    }
+
+
 }
 
 
